d0e412f5ccad91d0034864236c7f6bdd82a77afc
JeongHyeon <0429cjhn@naver.com>
2025-03-23 15:09:12 +0900

    feat: add pages folder

---
+++ src/pages/Admin/LoginPage.tsx
@@ -0,0 +1,7 @@
+import styles from "./LoginPage.module.less";
+
+const LoginPage = () => {
+  return <div className={styles.loginPage}>로그인페이지입니다.</div>;
+};
+
+export default LoginPage;
---
+++ src/pages/Landing/LandingPage.tsx
@@ -0,0 +1,7 @@
+import styles from "./LandingPage.module.less";
+
+const LandingPage = () => {
+  return <div className={styles.landingPage}>랜딩페이지입니다.</div>;
+};
+
+export default LandingPage;
