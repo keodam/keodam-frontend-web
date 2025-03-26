1679c3b2e47748e59d4afc25013c5368f166792e
JeongHyeon <0429cjhn@naver.com>
2025-03-26 18:49:30 +0900

    feat: add First frame

--- src/components/Landing/steps/First.tsx
+++ src/components/Landing/steps/First.tsx
@@ -1,7 +1,15 @@
+import { style } from "framer-motion/client";
 import styles from "./First.module.less";
 
 const First = () => {
-  return <div className={styles.first}>랜딩페이지에서 1페이지입니다.</div>;
+  return (
+    <div className={styles.first}>
+      <div className={styles.contents}>
+        <div className={styles.content}></div>
+        <div className={styles.assets}></div>
+      </div>
+    </div>
+  );
 };
 
 export default First;
