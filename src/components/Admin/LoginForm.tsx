40f588bbf39c0c64c1a063f89dbd82a176cff5fa
JeongHyeon <0429cjhn@naver.com>
2025-05-17 10:40:23 +0900

    feat: 로그인 폼 수정

--- src/components/Admin/LoginForm.tsx
+++ src/components/Admin/LoginForm.tsx
@@ -6,7 +6,7 @@ const LoginForm = () => {
   const [id, setId] = useState<string>("");
   const [passWord, setPassWord] = useState<string>("");
 
-  const handleId = (e: React.ChangeEvent<HTMLSelectElement>) =>
+  const handleId = (e: React.ChangeEvent<HTMLInputElement>) =>
     setId(e.target.value);
 
   const handlePassWord = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -22,8 +22,15 @@ const LoginForm = () => {
     <div>
       <form className={style.loginForm}>
         <div className={style.formIdPw}>
-          아이디:
-          <select
+          <input
+            type="text"
+            name="id"
+            placeholder="아이디"
+            className={style.pwInput}
+            value={id}
+            onChange={handleId}
+            />
+          {/* <select
             name="id"
             className={style.idSelect}
             value={id}
@@ -34,16 +41,15 @@ const LoginForm = () => {
             <option value="web">web</option>
             <option value="back">back</option>
             <option value="ai">ai</option>
-          </select>
+          </select> */}
         </div>
         {/* 비밀번호는 직접 입력해서 넣을 수 있도록 
             비밀번호 조건은 어떻게 만들 것인가. */}
         <div className={style.formIdPw}>
-          비밀번호:
           <input
             type="password"
             name="password"
-            placeholder="비밀번호를 입력하세요."
+            placeholder="비밀번호"
             className={style.pwInput}
             value={passWord}
             onChange={handlePassWord}
